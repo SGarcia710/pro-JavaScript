143ace011a46a6d0511b5f60350a09c146ca0302
Sebastián <sgarcia710@outlook.com>
2019-09-05 03:31:15 -0500

    Everything to TS

---
+++ assets/MediaPlayer.ts
@@ -0,0 +1,53 @@
+class MediaPlayer {
+  
+  media: HTMLMediaElement;
+  plugins: Array<any>;
+  
+  constructor(config: any) {
+    this.media = config.el;
+    this.plugins = config.plugins || [];
+    this.initPlugins();
+  }
+
+  private initPlugins() {
+    this.plugins.forEach(plugin => {
+      plugin.run(this);
+    });
+  }
+
+  play() {
+    this.media.play();
+  }
+
+  pause() {
+    this.media.pause();
+  }
+  
+  toggleVideo() {
+    if (this.media.paused === true) {
+      this.play();
+    }
+    else {
+      this.pause();
+    }
+  }
+  
+  mute() {
+    this.media.muted = true;
+  }
+  
+  unmute() {
+    this.media.muted = false;
+  }
+  
+  toggleAudio() {
+    if (this.media.muted) {
+      this.unmute();
+    }
+    else {
+      this.mute();
+    }
+  }
+}
+
+export default MediaPlayer;
