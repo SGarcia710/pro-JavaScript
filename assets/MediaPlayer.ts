f29d50ced259487b892e80566cfb16e7569b087e
Sebastián <sgarcia710@outlook.com>
2019-09-06 00:39:21 -0500

    ads added

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -2,11 +2,20 @@ class MediaPlayer {
   
   media: HTMLMediaElement;
   plugins: Array<any>;
+  container: HTMLElement;
   
   constructor(config: any ) {
     this.media = config.el;
     this.plugins = config.plugins || [];
+    this.initPlayer();
     this.initPlugins();
+  }
+
+  private initPlayer() {
+    this.container = document.createElement('div');// Así se crea un elemento en el dom
+    this.container.style.position = 'relative'; // Así se le añade un estilo a un elemento del dom
+    this.media.parentNode.insertBefore(this.container, this.media); // Ponemos el nuevo contenedor al lado de media
+    this.container.appendChild(this.media);//Y ponemos media dentro del contenedor
   }
 
   private initPlugins() {
